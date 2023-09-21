d0fec3a52a2a2f8e8cf75c6248282fb93a5cef26
ioamra <iom.monchau@gmail.com>
2023-09-21 15:28:16 +0200

    more css

--- connexion.php
+++ connexion.php
@@ -8,11 +8,11 @@
 <body>
     <?php require_once 'includes/bdd.php'; ?>
     <form method="post" class="container text-center">
-        <label class="w-100 d-block" for="mail">Mail</label>
-        <input class="w-100" type="email" name="mail">
-        <label class="w-100 d-block" for="password">Mot de passe</label>
-        <input class="w-100" type="password" name="password">
-        <button class="w-100" type="submit">Se connecter</button>
+        <label class="w-100 mb-1 d-block" for="mail">Mail</label>
+        <input class="w-100 mb-1" type="email" name="mail">
+        <label class="w-100 mb-1 d-block" for="password">Mot de passe</label>
+        <input class="w-100 mb-2" type="password" name="password">
+        <button class="w-100 mb-2" type="submit">Se connecter</button>
         <a href="inscription.php">Vous n'avez pas de compte, inscrivez-vous !</a>
     </form>
 </body>
